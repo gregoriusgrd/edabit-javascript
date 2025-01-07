0d766dd27b89a34eb1af14cc1f1074c7e9e6da81
gregoriusgrd <117911408+gregoriusgrd@users.noreply.github.com>
2025-01-07 14:39:17 +0700

    Add files via upload

---
+++ level-1/basketball-points/basketball-points.js
@@ -0,0 +1,8 @@
+// Basketball Points
+function points(twoPointers, threePointers) {
+    let finalPoints = (twoPointers * 2) + (threePointers * 3);
+    return finalPoints;
+  }
+  
+  console.log(points(1, 1))
+  console.log(points(7, 5))
