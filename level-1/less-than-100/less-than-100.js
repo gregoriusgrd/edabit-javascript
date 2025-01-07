5a7896adb284e8f63408d9a1bd5d0f1430ceecb9
gregoriusgrd <117911408+gregoriusgrd@users.noreply.github.com>
2025-01-07 15:21:28 +0700

    Add files via upload

---
+++ level-1/less-than-100/less-than-100.js
@@ -0,0 +1,12 @@
+// Less Than 100
+function lessThan100(a, b) {
+	const total = a + b;
+  if (total < 100){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(lessThan100(22, 15))
+console.log(lessThan100(83, 34))
